7667d046bd3714c267cb321493a785a82b9e1cf6
rubences <91731294+rubences@users.noreply.github.com>
2023-10-24 17:45:02 +0000

    cambios

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import copy
 
 
+
 class SelfReferencingEntity:
     def __init__(self):
         self.parent = None
 
     def set_parent(self, parent):
         self.parent = parent
+
 
 
 class SomeComponent:
